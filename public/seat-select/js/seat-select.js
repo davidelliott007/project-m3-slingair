8a7e7f9efb78cd25c40923f00819a3289e53b544
David Elliott <dave.elliott@gmail.com>
2020-07-08 22:47:00 -0400

    seat dropdown and seat population working

--- public/seat-select/js/seat-select.js
+++ public/seat-select/js/seat-select.js
@@ -2,11 +2,43 @@ const flightInput = document.getElementById('flight');
 const seatsDiv = document.getElementById('seats-section');
 const confirmButton = document.getElementById('confirm-button');
 
+let flightbuttons = [];
+let flights =[]
+let current_seats = [];
+let current_flight_to_display = [];
+
+function loadUpFlightButtons()
+{
+  console.log("loadUpFlightButtons ")
+  fetch(`/all-flights`)
+  .then((res) => res.json())
+  .then((data) => {
+
+    flights = data.allFlights;
+    flights.forEach(element =>
+      flightbuttons.push(document.getElementById(element))
+      );
+
+    
+    flightbuttons.forEach(element =>
+      {
+        element.addEventListener("click", flightClicked)
+      });
+
+  });
+}
+
+
+loadUpFlightButtons();
+
 let selection = '';
+
+
+
 
 const renderSeats = () => {
   document.querySelector('.form-container').style.display = 'block';
-
+ 
   const alpha = ['A', 'B', 'C', 'D', 'E', 'F'];
   for (let r = 1; r < 11; r++) {
     const row = document.createElement('ol');
@@ -21,8 +53,19 @@ const renderSeats = () => {
       const seatOccupied = `<li><label class="seat"><span id="${seatNumber}" class="occupied">${seatNumber}</span></label></li>`;
       const seatAvailable = `<li><label class="seat"><input type="radio" name="seat" value="${seatNumber}" /><span id="${seatNumber}" class="avail">${seatNumber}</span></label></li>`;
 
+      let seat_from_array = current_seats.find(element => element.id === seatNumber);
+
+      if (seat_from_array.isAvailable)
+{
+  seat.innerHTML = seatAvailable;
+
+}
+else{
+  seat.innerHTML = seatOccupied;
+
+}
+
       // TODO: render the seat availability based on the data...
-      seat.innerHTML = seatAvailable;
       row.appendChild(seat);
     }
   }
@@ -74,4 +117,34 @@ const handleConfirmSeat = (event) => {
   });
 };
 
-flightInput.addEventListener('blur', toggleFormContent);
+function flightClicked()
+{
+  // console.log(event);
+  //console.log(event.target.id)
+
+  fetch(`/flights/${event.target.id}`)
+  .then((res) => res.json())
+  .then((data) => {
+    // console.log(data);
+    current_seats = data.flight_to_return[1];
+    current_flight_to_display = data.flight_to_return[0];
+
+     console.log(current_flight_to_display);
+
+     seatsDiv.querySelectorAll('*').forEach(n => n.remove());
+    renderSeats();
+
+  });
+
+}
+
+//flightInput.addEventListener('blur', toggleFormContent);
+
+
+function myFunction()
+{
+  console.log("hello myfuction");
+
+
+
+}
--- server.js
+++ server.js
@@ -6,14 +6,75 @@ const morgan = require('morgan');
 const { flights } = require('./test-data/flightSeating');
 
 const PORT = process.env.PORT || 8000;
+let url = require('url');
+
+
 
 const handleFlight = (req, res) => {
   const { flightNumber } = req.params;
   // get all flight numbers
   const allFlights = Object.keys(flights);
   // is flightNumber in the array?
-  console.log('REAL FLIGHT: ', allFlights.includes(flightNumber));
+
+  let flight_to_return;
+
+
+  for (let flightObject of Object.entries(flights)) {
+    if (flightObject[0] === flightNumber)
+
+    {
+      console.log("found flight");
+
+    console.log(flightObject[0])
+    console.log(flightObject[1])
+    flight_to_return = flightObject;
+
+    }
+  }
+
+
+
+
+  // console.log(flight_to_return);
+  // console.log(flightObject);
+  res.status(200).send({flight_to_return:flight_to_return});
+
+  //console.log('REAL FLIGHT: ', allFlights.includes(flightNumber));
 };
+
+const renderSeatSelect = (req, res) => 
+{
+  let url1 = getFormattedUrl(req);
+  console.log(url1+"/dave");
+  //http://localhost:8000/flights/SA123
+  const allFlights = Object.keys(flights);
+
+  let allflightsWithLinks = allFlights.map(element => {
+  return {flight:element, link:url1+"/flights/"+element}
+  }  
+  );
+
+  
+  
+  console.log(allflightsWithLinks);
+    res.status(200).render('pages/seat-select', {flights:allflightsWithLinks});
+}
+
+const returnAllFlights = (req, res) => 
+{
+  const allFlights = Object.keys(flights);
+
+    res.status(200).send({allFlights:allFlights});
+}
+
+
+
+function getFormattedUrl(req) {
+  return url.format({
+      protocol: req.protocol,
+      host: req.get('host')
+  });
+}
 
 express()
   .use(function (req, res, next) {
@@ -28,8 +89,12 @@ express()
   .use(express.static('public'))
   .use(bodyParser.json())
   .use(express.urlencoded({ extended: false }))
+  .set('view engine', 'ejs')
 
   // endpoints
   .get('/flights/:flightNumber', handleFlight)
+  .get('/seat-select', renderSeatSelect)
+  .get('/all-flights', returnAllFlights)
+
   .use((req, res) => res.send('Not Found'))
   .listen(PORT, () => console.log(`Listening on port ${PORT}`));
